c7fda4851f06d48b72b12d6f604a89a623206b4b
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-06-08 11:28:10 -0500

    swirl1

---
+++ swirl1.R
@@ -0,0 +1,14 @@
+#SWIRL EXERCISES
+install.packages("swirl")
+library(swirl)
+install.packages("R Programming")
+swirl()
+#You can exit swirl and return to the R prompt (>) at any time
+#by pressing the Esc key If you are already at the prompt, type
+# bye() to exit and save your progress. When you exit properly,
+# you'll see a short message letting you know you've done so.
+bye()
+swirl()
+5+7
+
+
