70499b85fb7807aca75b7b4c1d3a0bdade5936f7
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-06-08 20:19:29 -0500

    SUBSETTING

---
+++ L4.R
@@ -0,0 +1,62 @@
+# SUBSETTING 
+# operators :[], [[]], $
+# [] returns an object of the same class as the original 
+# [[]] to extracts of a list or a data frame a single element 
+# $ extract elements by names similar to [[]] 
+
+# vector
+x <- c("a","b")
+x[1] #subset by numeric 
+x[1:2]
+
+x[x > "a"] #subset by a logical 
+
+u <- x > "a"
+x[u]
+
+# list 
+
+x <- list(foo = 1:4, bar = 0.6)
+x[1] # i get a list --- always return the same class as the original object 
+
+x[[1]] # i get just the vector
+
+
+x$bar
+x$foo
+
+#extract multiple elementes
+x[1:2]
+x[c(1,2)]
+
+# [[]] can be used with computed indices; $ can only be used with literal
+
+x[[1]][[2]]
+x[[c(1,3)]]
+
+# matrices
+
+x <- matrix(1:6, 2,3)
+x
+x[1,2]  # by default it retrives a vector! not matrix 
+x[2,1]
+x[,1]
+x[1,]
+
+# if i what to retrive a matrix
+
+ x[1,2, drop = FALSE]
+ x[1, , drop = FALSE]
+ 
+ 
+ # PARTIAL MATCHING --- a tip 
+ 
+x <- data.frame(foo =1:4, bar =c(T,T,F,F)) 
+ 
+x$f 
+x$b  # it works with only one letter!!!!
+x[["a"]]
+
+ 
+ 
+ 
