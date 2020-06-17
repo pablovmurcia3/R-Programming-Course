5b03c174c6448eaa8aebc1b83f120d7a3ae06f5f
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-06-17 09:13:57 -0500

    swirl2

--- swirl2.R
+++ swirl2.R
@@ -229,3 +229,68 @@ mad_libs(place='New Zealand', adjective='beautiful', noun='landscape')
 
 
 "I"%p%"love"%p%"R!"
+################################################################################
+
+swirl()
+
+# dates are stored as the number of days since 1970-01-01 and times are stored 
+# as either the number of seconds since 1970-01-01 (for 'POSIXct') or a list
+# of seconds, minutes, hours, etc. (for 'POSIXlt').
+
+d1 <- Sys.Date()
+
+class(d1)
+
+unclass(d1)
+
+d1
+
+d2 <- as.Date("1969-01-01")
+
+unclass(d2)
+
+t1 <- Sys.time()
+
+t1
+
+class(t1)
+
+unclass(t1)
+
+t2 <- as.POSIXlt(Sys.time())
+
+class(t2)
+
+t2
+
+unclass(t2)
+
+str(unclass(t2))
+
+t2$min
+
+weekdays(d1)
+
+months(t1)
+
+quarters(t2)
+
+strptime()
+# strptime() converts character vectors to POSIXlt
+
+t3 <- "October 17, 1986 08:24" 
+
+t4 <- strptime(t3, "%B %d, %Y %H:%M") 
+
+t4
+
+class(t4)
+
+Sys.time() > t1
+
+Sys.time() - t1
+
+difftime(Sys.time(), t1, units = 'days')
+
+# lubridate package by Hadley Wickham
+
