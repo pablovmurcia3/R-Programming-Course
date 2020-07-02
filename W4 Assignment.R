20ae99065a0f4c265e4f9b86aa57014ceadc963b
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-07-02 11:56:59 -0500

    W4 assignment

--- W4 Assignment.R
+++ W4 Assignment.R
@@ -187,10 +187,10 @@ rankhospital <- function(state,outcome, num = "best") {
 }
   
 
-rankhospital("MD","heart attack", "worst")
 rankhospital("TX","heart failure", 4)
-rankhospital("MD","heart attack", 5000)
-rankhospital("TX","pneumonia",302)
+rankhospital("MD","heart attack", "worst")
+rankhospital("TX","pneumonia",32)
+rankhospital("MD","heart attack",5000)
 
 # 3
 
@@ -321,5 +321,22 @@ head(rankall("heart attack", 20),10)
 tail(rankall("pneumonia","worst"),3)
 tail(rankall("heart failure"),10)
 
+###############################################################################
 
+best("SC", "heart attack")
+best("NY", "pneumonia")
+best("AK", "pneumonia")
 
+rankhospital("NC", "heart attack", "worst")
+rankhospital("WA", "heart attack", 7)
+rankhospital("TX", "pneumonia", 10)
+rankhospital("NY", "heart attack", 7)
+
+r <- rankall("heart attack", 4)
+as.character(subset(r, state == "HI")$hospital)
+
+r <- rankall("pneumonia", "worst")
+as.character(subset(r, state == "NJ")$hospital)
+
+r <- rankall("heart failure", 10)
+as.character(subset(r, state == "NV")$hospital)
